97f8facb687bdc4bd7968fd7e85bad814634c37a
Mark Fassler <mfassler@users.noreply.github.com>
2019-09-11 20:58:11 +0900

    update to new packet structure from Moab

--- testing_tools/host_imu_receiver.py
+++ testing_tools/host_imu_receiver.py
@@ -29,15 +29,20 @@ this is the packet structure in C:
         int16_t _padding1;  // the compiler seems to like 64-bit boundaries
         char bnoData[20];  // internal IMU
 
-        float temperature;
+        float temperature; // degrees celsius, no need for high accuracy
+
+        // Pressure:  typical sensor value is ~100000, with accuracy of +/- 12.0,
+        // (don't forget to convert between Pa and hPa), so this is well
+        // within the accuracy of float32
         float pressure;
+
+        uint16_t sbus_a;
+        uint16_t sbus_b;
 
         // TODO:  do we really need float64 for these numbers?
         double shaft_pps;
 
-        int32_t _padding2;  // the compiler seems to like 64-bit boundaries
     } mData;
-
 '''
 
 
@@ -46,8 +51,14 @@ while True:
     if len(pkt) != 48:
         print("udp packet is wrong length:", len(pkt))
     else:
-        magX, magY, magZ, _nothing1, qw, qx, qy, qz, lax, lay, laz, gx, gy, gz, temperature, pressure, shaft_pps, _nothing2 = struct.unpack("<hhhhhhhhhhhhhhffdl", pkt)
+
+        magX, magY, magZ, _nothing1, \
+        qw, qx, qy, qz, lax, lay, laz, gx, gy, gz, \
+        temperature, pressure, sbus_a, sbus_b, shaft_pps \
+            = struct.unpack("<hhhhhhhhhhhhhhffHHd", pkt)
+
         print(magX, magY, magZ, lax, lay, laz, gx, gy, gz, shaft_pps, temperature, pressure)
+        #print(shaft_pps)
 
         rot = transforms3d.quaternions.quat2mat([qw, qx, qy, qz])
 
