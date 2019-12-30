3f1d3006026da83230747d2494bb7ab14ce79c8f
Mark Fassler <mfassler@users.noreply.github.com>
2019-12-30 13:58:38 +0900

    moar logging!

--- testing_tools/capture_packets.py
+++ testing_tools/capture_packets.py
@@ -12,31 +12,56 @@ import pickle
 
 NMEA_RX_PORT = 27113
 nmea_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+nmea_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
 nmea_sock.bind(("0.0.0.0", NMEA_RX_PORT))
 
-BNO055_RX_PORT = 27114
+IMU_RX_PORT = 27114
 imu_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-imu_sock.bind(("0.0.0.0", BNO055_RX_PORT))
+imu_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+imu_sock.bind(("0.0.0.0", IMU_RX_PORT))
 
 PID_CONTROL_PORT = 27311
 pid_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+pid_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
 pid_sock.bind(("0.0.0.0", PID_CONTROL_PORT))
+
+MOAB_STATUS_PORT = 31337
+moab_status_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+moab_status_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+moab_status_sock.bind(("0.0.0.0", MOAB_STATUS_PORT))
+
+SBUS_RX_PORT = 31338
+sbus_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sbus_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+sbus_sock.bind(("0.0.0.0", IMU_RX_PORT))
+
 
 f = open(sys.argv[1], 'wb')
 
+all_sockets = [nmea_sock, imu_sock, pid_sock, moab_status_sock, sbus_sock]
 while True:
-    inputs, outputs, errors = select.select([nmea_sock, imu_sock, pid_sock], [], [])
+    inputs, outputs, errors = select.select(all_sockets, [], [])
     for oneInput in inputs:
         if oneInput == nmea_sock:
-            data, addr = nmea_sock.recvfrom(1500)
-            pickle.dump(('nmea', time.time(), addr, data), f, -1)
-            print('nmea', len(data))
+            pkt, addr = nmea_sock.recvfrom(1500)
+            pickle.dump(('nmea', time.time(), addr, pkt), f, -1)
+            print('nmea', len(pkt))
         elif oneInput == imu_sock:
-            data, addr = imu_sock.recvfrom(1500)
-            pickle.dump(('imu', time.time(), addr, data), f, -1)
-            print('imu', len(data))
+            pkt, addr = imu_sock.recvfrom(1500)
+            pickle.dump(('imu', time.time(), addr, pkt), f, -1)
+            print('imu', len(pkt))
         elif oneInput == pid_sock:
-            data, addr = pid_sock.recvfrom(1500)
-            pickle.dump(('pid', time.time(), addr, data), f, -1)
-            print('pid', len(data))
+            pkt, addr = pid_sock.recvfrom(1500)
+            pickle.dump(('pid', time.time(), addr, pkt), f, -1)
+            print('pid', len(pkt))
+        elif oneInput == moab_status_sock:
+            pkt, addr = moab_status_sock.recvfrom(1500)
+            pickle.dump(('moab', time.time(), addr, pkt), f, -1)
+            print('moab', len(pkt))
+        elif oneInput == sbus_sock:
+            pkt, addr = sbus_sock.recvfrom(1500)
+            pickle.dump(('sbus', time.time(), addr, pkt), f, -1)
+            print('sbus', len(pkt))
+
+
 
--- testing_tools/parsePackets.py
+++ testing_tools/parsePackets.py
@@ -6,6 +6,8 @@ if sys.version_info[0] < 3:
 
 import pickle
 import struct
+import numpy as np
+import matplotlib.pyplot as plt
 
 from ImuPacket import ImuPacket
 imu = ImuPacket()
@@ -26,6 +28,11 @@ packets = []
 f = open(sys.argv[1], 'rb')
 
 
+nmea_vals = []
+imu_vals = []
+pid_vals = []
+moab_vals = []
+sbus_vals = []
 
 def parse_obj(obj):
     assert len(obj) == 4
@@ -38,12 +45,22 @@ def parse_obj(obj):
         # raw_packet is an ASCII NMEA sentence:
         nmea_sentence = raw_packet
         # do something useful here
+        nmea_vals.append((timestamp, nmea_sentence))
     elif protocol == 'imu':
         imu.parse(raw_packet)
         # do something useful here
+        imu_vals.append((timestamp, imu.shaft_a_pps, imu.shaft_b_pps))
     elif protocol == 'pid':
         pid.parse(raw_packet)
         # do something useful here
+        pid_vals.append((timestamp, 
+                          pid._sbus_steering, pid._sbus_throttle, pid.output, \
+                          pid.K_p, pid._e, pid.K_i, pid._I, \
+                          pid._target_speed, pid._actual_speed))
+    elif protocol == 'moab':
+        moab_vals.append((timestamp, raw_packet))
+    elif protocol == 'sbus':
+        sbus_vals.append((timestamp, raw_packet))
     else:
         print("Unknown protocol:", protocol)
 
@@ -57,6 +74,10 @@ while True:
     except EOFError:
         break
 
+
+
+imu_data = np.array(imu_vals)
+pid_data = np.array(pid_vals)
 
 
 
