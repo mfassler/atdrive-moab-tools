e43424ff0bd602615788fd8e702aba5942c80584
Mark Fassler <mfassler@users.noreply.github.com>
2020-02-17 11:11:18 +0900

    config files

---
+++ ROBOT_CONFIG__office.py
@@ -0,0 +1,30 @@
+
+
+MOAB_IP_ADDRESS = "192.168.1.201"
+#MAVLINK_IP_ADDRESS = "192.168.1.36"
+MAVLINK_IP_ADDRESS = "127.0.0.1"
+
+Nav_Recipients = [
+    ('127.0.0.1', 27201),
+    #('127.0.0.1', 27202),
+]
+
+
+SHAFT_ENCODER_DISTANCE = 0.09  # 9 cm per tick
+
+compass_x_range = 235 / 2.0
+compass_y_range = 235 / 2.0
+compass_z_range = 235 / 2.0
+
+compass_x_center = -52.5
+compass_y_center = 48.0
+compass_z_center = -77.0
+
+mag_declination = -7.57  # Tokyo
+
+
+lidar_ref_angle = 62.4
+lidar_ref_lat = 35.6535
+lidar_ref_lon = 139.837
+
+
---
+++ ROBOT_CONFIG_palmtrees.py
@@ -0,0 +1,28 @@
+
+
+MOAB_IP_ADDRESS = "192.168.21.201"
+MAVLINK_IP_ADDRESS = "192.168.21.80"
+
+Nav_Recipients = [
+    #('127.0.0.1', 27201),
+    ('192.168.21.255', 27201),
+]
+
+
+SHAFT_ENCODER_DISTANCE = 0.09  # 9 cm per tick
+
+compass_x_range = 3100
+compass_y_range = 2567
+compass_z_range = 2900
+
+compass_x_center = -3847
+compass_y_center = 1200
+compass_z_center = -77.0
+
+mag_declination = 0.0
+
+lidar_ref_angle = 45.0
+lidar_ref_lat = 35.6535
+lidar_ref_lon = 139.837
+
+
